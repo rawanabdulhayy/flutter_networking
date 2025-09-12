ebafddec5ed117e90e9da62d984fa6dfcaa9eb49
rawanabdulhayy <rawanabdulhay29700@gmail.com>
2025-09-12 19:43:41 +0300

    now playing widget bloc works now

--- lib/logic/now_playing/now_playing_bloc.dart
+++ lib/logic/now_playing/now_playing_bloc.dart
@@ -1,13 +1,33 @@
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:dio/dio.dart';
 
-part 'now_playing_event.dart';
-part 'now_playing_state.dart';
+import '../../models/movie.dart';
+import 'now_playing_event.dart';
+import 'now_playing_state.dart';
 
 class NowPlayingBloc extends Bloc<NowPlayingEvent, NowPlayingState> {
-  NowPlayingBloc() : super(NowPlayingInitial()) {
-    on<NowPlayingEvent>((event, emit) {
-      // TODO: implement event handler
+  final Dio dio;
+  //super takes the state of the parent widget.
+  NowPlayingBloc(this.dio) : super(NowPlayingInitial()) {
+    on<FetchNowPlayingMovies>((event, emit) async{
+      emit(NowPlayingLoading());
+      try{
+        //request, endpoint path passed in a get/post function through dio's object.
+        //async await block awaits the dio's response.
+        final response = await dio.get("https://api.themoviedb.org/3/movie/now_playing?api_key=87903828b97a85b50c60fb3bbd960c55");
+        if(response.statusCode == 200){
+          //1- a3mel t7wel l from json
+          final movieResponse = Movies.fromJson(response.data);
+          //2- emit the loaded success state, returns the converted json variable
+          emit(NowPlayingLoaded(movieResponse));
+        }
+        else{
+          emit(NowPlayingError("Failed"));
+        }
+      }
+      catch (e){
+        emit(NowPlayingError(e.toString()));
+      }
     });
   }
 }
